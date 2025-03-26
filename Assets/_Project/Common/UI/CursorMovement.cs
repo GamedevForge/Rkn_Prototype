2dfcc98487df33e37258452a2eaa42ce9d2eeb60
alexandrKozhenyazov <whiteminecraft90@gmail.com>
2025-03-26 23:55:09 +0500

    cursor movement rework

--- Assets/_Project/Common/UI/CursorMovement.cs
+++ Assets/_Project/Common/UI/CursorMovement.cs
@@ -12,23 +12,22 @@ namespace Project.Common.UI
         private float _xOffSet = 0f;
         private float _yOffSet = 0f;
 
-        private float halfSize => GetComponent<RectTransform>().sizeDelta.x / 2f;
+        private float HalfSize => GetComponent<RectTransform>().sizeDelta.x / 2f;
 
         private void Update()
         {
-            _xOffSet += _assetsInputs.look.x * _mouseSensivity;
-            _yOffSet += _assetsInputs.look.y * _mouseSensivity;
+            _xOffSet = Mathf.Clamp(_xOffSet + _assetsInputs.look.x * _mouseSensivity, 
+                -_monitorCanvasRectTransform.sizeDelta.x / 2 + HalfSize, 
+                _monitorCanvasRectTransform.sizeDelta.x / 2 - HalfSize);
+            
+            _yOffSet = Mathf.Clamp(_yOffSet + _assetsInputs.look.y * _mouseSensivity, 
+                -_monitorCanvasRectTransform.sizeDelta.y / 2 + HalfSize, 
+                _monitorCanvasRectTransform.sizeDelta.y / 2 - HalfSize);
 
             transform.localPosition = new Vector3(
-                Mathf.Clamp(_xOffSet, -_monitorCanvasRectTransform.sizeDelta.x / 2 + halfSize, _monitorCanvasRectTransform.sizeDelta.x / 2 - halfSize), 
-                -Mathf.Clamp(_yOffSet, -_monitorCanvasRectTransform.sizeDelta.y / 2 + halfSize, _monitorCanvasRectTransform.sizeDelta.y / 2 - halfSize), 
+                _xOffSet, 
+                -_yOffSet, 
                 transform.localPosition.z);
         }
-        /*
-        private void Update()
-        {
-            transform.position = _camera.ScreenToWorldPoint(Input.mousePosition);
-        }
-        */
     }
 }
