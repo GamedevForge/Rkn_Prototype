a09b8243c01e1c5e00a63b8f317524a06bfe0ee7
alexandrKozhenyazov <whiteminecraft90@gmail.com>
2025-04-08 16:03:44 +0500

    rework cursor logic

--- Assets/_Project/Common/UI/CursorBaseRaycaster.cs
+++ Assets/_Project/Common/UI/CursorBaseRaycaster.cs
@@ -141,15 +141,45 @@ namespace Project.Common.UI
             else
                 displayIndex = currentEventCamera.targetDisplay;
 
+            eventData.position = currentEventCamera.WorldToScreenPoint(_cursorTransform.position);
+            Vector3 eventPosition = GetRelativeMousePositionForRaycast(eventData);
+
+            Debug.Log(eventPosition);
+            if ((int)eventPosition.z != displayIndex)
+                return;
+
+            // Convert to view space
+            Vector2 pos;
+            if (currentEventCamera == null)
+            {
+                // Multiple display support only when not the main display. For display 0 the reported
+                // resolution is always the desktops resolution since its part of the display API,
+                // so we use the standard none multiple display method. (case 741751)
+                float w = Screen.width;
+                float h = Screen.height;
+                if (displayIndex > 0 && displayIndex < Display.displays.Length)
+                {
+                    w = Display.displays[displayIndex].systemWidth;
+                    h = Display.displays[displayIndex].systemHeight;
+                }
+                pos = new Vector2(eventPosition.x / w, eventPosition.y / h);
+            }
+            else
+                pos = currentEventCamera.ScreenToViewportPoint(eventPosition);
+
+            // If it's outside the camera's viewport, do nothing
+            if (pos.x < 0f || pos.x > 1f || pos.y < 0f || pos.y > 1f)
+                return;
+
             float hitDistance = float.MaxValue;
 
             Ray ray = new();
 
             if (currentEventCamera != null)
             {
-                ray = new(_cursorTransform.position, Vector3.forward);
+                ray = currentEventCamera.ScreenPointToRay(eventPosition);
             }
-            Debug.DrawRay(_cursorTransform.position, Vector3.forward);
+            Debug.DrawRay(ray.origin, ray.direction);
 
             if (canvas.renderMode != RenderMode.ScreenSpaceOverlay && blockingObjects != BlockingObjects.None)
             {
@@ -190,7 +220,7 @@ namespace Project.Common.UI
 
             m_RaycastResults.Clear();
 
-            Raycast(canvas, currentEventCamera, _cursorTransform.position, canvasGraphics, m_RaycastResults);
+            Raycast(canvas, currentEventCamera, eventPosition, canvasGraphics, m_RaycastResults);
 
             int totalCount = m_RaycastResults.Count;
             for (var index = 0; index < totalCount; index++)
@@ -292,12 +322,8 @@ namespace Project.Common.UI
                 if (!graphic.raycastTarget || graphic.canvasRenderer.cull || graphic.depth == -1)
                     continue;
 
-                //Debug.Log(pointerPosition);
-                if (!RectTransformUtility.RectangleContainsScreenPoint(graphic.rectTransform, pointerPosition))
+                if (!RectTransformUtility.RectangleContainsScreenPoint(graphic.rectTransform, pointerPosition, eventCamera, graphic.raycastPadding))
                     continue;
-
-                //if(!CursorOverTheObject(graphic.rectTransform, pointerPosition, canvas.transform))
-                    //continue;
 
                 if (eventCamera != null && eventCamera.WorldToScreenPoint(graphic.rectTransform.position).z > eventCamera.farClipPlane)
                     continue;
@@ -316,17 +342,118 @@ namespace Project.Common.UI
             s_SortedGraphics.Clear();
         }
 
-        private static bool CursorOverTheObject(RectTransform uIRectTransform, Vector2 cursorPosition, Transform canvasTransform)
+        internal static Vector3 GetRelativeMousePositionForRaycast(PointerEventData eventData)
         {
-            if (uIRectTransform.rect.Contains(cursorPosition))
-                return true;
-            else
-                return false;
+            // The multiple display system is not supported on all platforms, when it is not supported the returned position
+            // will be all zeros so when the returned index is 0 we will default to the event data to be safe.
+            Vector3 eventPosition = RelativeMouseAtScaled(eventData.position, eventData.displayIndex);
+            if (eventPosition == Vector3.zero)
+            {
+                eventPosition = eventData.position;
+#if UNITY_EDITOR
+                eventPosition.z = Display.activeEditorGameViewTarget;
+#endif
+                // We don't really know in which display the event occurred. We will process the event assuming it occurred in our display.
+            }
 
-            /*float distance = Vector2.Distance(uIRectTransform.position, cursorPosition);
+            // We support multiple display on some platforms. When supported:
+            //  - InputSystem will set eventData.displayIndex
+            //  - Old Input System will set eventPosition.z
+            //
+            // If the event is on the main display, both displayIndex and eventPosition.z
+            // will be 0 so in that case we can leave the eventPosition untouched (see UUM-47650).
+#if ENABLE_INPUT_SYSTEM && PACKAGE_INPUTSYSTEM
+            if (eventData.displayIndex > 0)
+                eventPosition.z = eventData.displayIndex;
+#endif
 
-            return distance < (uIRectTransform.sizeDelta.x / 2f) * canvasTransform.localScale.x * uIRectTransform.localScale.x
-                || distance < (uIRectTransform.sizeDelta.y / 2f) * canvasTransform.localScale.y * uIRectTransform.localScale.y;*/
+            return eventPosition;
+        }
+
+        /// <summary>
+        /// A version of Display.RelativeMouseAt that scales the position when the main display has a different rendering resolution to the system resolution.
+        /// By default, the mouse position is relative to the main render area, we need to adjust this so it is relative to the system resolution
+        /// in order to correctly determine the position on other displays.
+        /// </summary>
+        /// <returns></returns>
+        public static Vector3 RelativeMouseAtScaled(Vector2 position, int displayIndex)
+        {
+#if !UNITY_EDITOR && !UNITY_WSA
+            // If the main display is now the same resolution as the system then we need to scale the mouse position. (case 1141732)
+            if (Display.main.renderingWidth != Display.main.systemWidth || Display.main.renderingHeight != Display.main.systemHeight)
+            {
+                // The system will add padding when in full-screen and using a non-native aspect ratio. (case UUM-7893)
+                // For example Rendering 1920x1080 with a systeem resolution of 3440x1440 would create black bars on each side that are 330 pixels wide.
+                // we need to account for this or it will offset our coordinates when we are not on the main display.
+                var systemAspectRatio = Display.main.systemWidth / (float)Display.main.systemHeight;
+
+                var sizePlusPadding = new Vector2(Display.main.renderingWidth, Display.main.renderingHeight);
+                var padding = Vector2.zero;
+                if (Screen.fullScreen)
+                {
+                    var aspectRatio = Screen.width / (float)Screen.height;
+                    if (Display.main.systemHeight * aspectRatio < Display.main.systemWidth)
+                    {
+                        // Horizontal padding
+                        sizePlusPadding.x = Display.main.renderingHeight * systemAspectRatio;
+                        padding.x = (sizePlusPadding.x - Display.main.renderingWidth) * 0.5f;
+                    }
+                    else
+                    {
+                        // Vertical padding
+                        sizePlusPadding.y = Display.main.renderingWidth / systemAspectRatio;
+                        padding.y = (sizePlusPadding.y - Display.main.renderingHeight) * 0.5f;
+                    }
+                }
+
+                var sizePlusPositivePadding = sizePlusPadding - padding;
+
+                // If we are not inside of the main display then we must adjust the mouse position so it is scaled by
+                // the main display and adjusted for any padding that may have been added due to different aspect ratios.
+                if (position.y < -padding.y || position.y > sizePlusPositivePadding.y ||
+                     position.x < -padding.x || position.x > sizePlusPositivePadding.x)
+                {
+                    var adjustedPosition = position;
+
+                    if (!Screen.fullScreen)
+                    {
+                        // When in windowed mode, the window will be centered with the 0,0 coordinate at the top left, we need to adjust so it is relative to the screen instead.
+                        adjustedPosition.x -= (Display.main.renderingWidth - Display.main.systemWidth) * 0.5f;
+                        adjustedPosition.y -= (Display.main.renderingHeight - Display.main.systemHeight) * 0.5f;
+                    }
+                    else
+                    {
+                        // Scale the mouse position to account for the black bars when in a non-native aspect ratio.
+                        adjustedPosition += padding;
+                        adjustedPosition.x *= Display.main.systemWidth / sizePlusPadding.x;
+                        adjustedPosition.y *= Display.main.systemHeight / sizePlusPadding.y;
+                    }
+
+					// fix for UUM-63551: Use the display index provided to this method.  Display.RelativeMouseAt( ) no longer works starting with 2021 LTS and new input system
+					// as the Pointer position is reported in Window coordinates rather than relative to the primary window as Display.RelativeMouseAt( ) expects.
+#if ENABLE_INPUT_SYSTEM && PACKAGE_INPUTSYSTEM && (UNITY_STANDALONE_WIN || UNITY_STANDALONE_LINUX)
+                    var relativePos = new Vector3(adjustedPosition.x, adjustedPosition.y, displayIndex);
+#else
+                    var relativePos = Display.RelativeMouseAt(adjustedPosition);
+#endif
+
+                    // If we are not on the main display then return the adjusted position.
+                    if (relativePos.z != 0)
+                        return relativePos;
+                }
+
+                // We are using the main display.
+                return new Vector3(position.x, position.y, 0);
+            }
+#endif
+
+            // fix for UUM-63551: Use the display index provided to this method.  Display.RelativeMouseAt( ) no longer works starting with 2021 LTS and new input system
+            // as the Pointer position is reported in Window coordinates rather than relative to the primary window as Display.RelativeMouseAt( ) expects.
+#if ENABLE_INPUT_SYSTEM && PACKAGE_INPUTSYSTEM && (UNITY_STANDALONE_WIN || UNITY_STANDALONE_LINUX)
+            return new Vector3(position.x, position.y, displayIndex);
+#else
+            return Display.RelativeMouseAt(position);
+#endif
         }
     }
 }
